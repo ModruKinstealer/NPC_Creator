cbe9d900224c09c40d11eb4c544610364c6ed61a
“Ray <modrukinstealer@hotmail.com>
2022-09-28 10:20:16 -0400

    Change password working as intended.

--- app.py
+++ app.py
@@ -44,18 +44,18 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("Must provide username", 403)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("Must provide password", 403)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE name = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["pwhash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return apology("Invalid username and/or password", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -66,7 +66,7 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-        
+
 
 @app.route("/logout")
 def logout():
@@ -125,7 +125,7 @@ def register():
         user = db.execute("SELECT * FROM users WHERE name = ?", request.form.get("username"))
         session["user_id"] = user[0]["id"]
         return redirect("/")
-        
+
     else:
         return render_template("register.html")
 
@@ -141,7 +141,7 @@ def password():
             return apology("Must provide all current password, new password, and confirm new password", 403)
 
         # Passwords should be 8 or more characters long
-        elif len(request.form.get("newPassword")) < 8 or len(request.form.get("confirmation")) < 8: 
+        elif len(request.form.get("newPassword")) < 8 or len(request.form.get("confirmation")) < 8:
             return apology("Passwords must contain 8 or more characters.")
 
         # Ensure new password and confirm password match
@@ -153,7 +153,7 @@ def password():
             return apology("New password can not be the same as old password.")
         print(request.form.get("oldPassword"))
         print(request.form.get("newPassword"))
-        
+
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])
 
