d12322b1197f356e7c025db73a1c8b89695805b6
“Ray <modrukinstealer@hotmail.com>
2022-11-01 15:38:30 -0400

    Moving logging to a Todo, my not need.

--- spells_import.py
+++ spells_import.py
@@ -1,7 +1,7 @@
 import json as JSON
-import logging
+
 from cs50 import SQL
-import datetime
+ # If use logging
 
 # Function to take a json file and INSERT any spells in it into spells db
 def spells_import(file, user, db):
@@ -11,47 +11,63 @@ def spells_import(file, user, db):
     with open(file) as file:
         data = JSON.load(file)
 
-    # Configure some logging
     '''
+    TODO: Implment logging? Is logging really needed?
+    # If decide to use logging Add these two to imports
+    import datetime
+    import logging
+
+    # Configure some logging
     date = datetime.datetime.now()
     date = date.strftime("%d-%m-%Y %H:%M:%S")
     Dynamically create log name with user and date timestamp
-    '''
+
+    # Create a list for errors, each error will be a list with the name of the spell and which part of the import process didn't work I.E. ['magic missile', description]
+    errors = []
 
     log_name =
     logging.basicConfig(filename='example.log', encoding='utf-8')
 
-    # Create a dict to store spells with the format intended for feeding to the jinja templates
+    # Try / except where if it doesn't work add name and type to a list to print out at end?
+    '''
+
+    # Create a list to store spells with the format intended for feeding to the jinja templates
     spells = []
-    # Create a list for errors, each error will be a list with the name of the spell and which part of the import process didn't work I.E. ['magic missile', description]
-    errors = []
+
     # Iterate through data and build a new dict with just the info needed for our db
     for spell in data:
         if spell["type"] == "spell":
             # Empty dict to populate for each iteration
-            print(spell["name"])
-            print(spell["img"])
-            print(spell["data"]["description"]["value"])
-            print(spell["data"]["source"])
-            print(spell["data"]["level"])
-            print(spell["data"]["school"])
-            print(spell["data"]["components"]["vocal"])
-            print(spell["data"]["components"]["somatic"])
-            print(spell["data"]["components"]["material"])
-            print(spell["data"]["components"]["ritual"])
-            print(spell["data"]["components"]["concentration"])
+            this_spell = {}
 
-            # TODO: logging? try except where if it doesn't work add name and type to a list to print out at end.
-            # TODO: Field split into dict with multiple keys, verify from Export of individual item before finalizing
+            # Some keys/values will be straight adds
+            this_spell["name"] = spell["name"]
+            this_spell["lvl"] = spell["data"]["level"]
+            this_spell["img"] = spell["img"]
+            this_spell["description"] = spell["data"]["description"]["value"]
+            this_spell["school"] = spell["data"]["school"]
+            this_spell["verbal"] = spell["data"]["components"]["vocal"]
+            this_spell["somatic"] = spell["data"]["components"]["somatic"]
+            this_spell["material"] = spell["data"]["components"]["material"]
+            this_spell["ritual"] = spell["data"]["components"]["ritual"]
+            this_spell["concentration"] = spell["data"]["components"]["concentration"]
+
+            # Check sources table, add if needed, get id for the source, add that as value
+            print(spell["data"]["source"])
+
+            # These keys were split into multiple values so have to be combined into format we want to use
             '''
-            print(spell["data"]["materials"]["value"])
-            print(spell["data"]["activation"]["type"])
+            classes
+            print(spell["data"]["materials"]["value"]) # value is text of what material component is?
+            print(spell["data"]["activation"]["type"]) # casting time?
             print(spell["data"]["duration"]["value"])
             print(spell["data"]["target"]["value"])
+            save
+            dmg
+            
             '''
 
-
-    # return(spells)
+    #return(spells)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///npc.db")
